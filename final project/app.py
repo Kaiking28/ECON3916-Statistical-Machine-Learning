af6254b050483c37fa1ad1fd78f3553c9fbaf34e
Kai King <king.kai@northeastern.edu>
2026-04-26 16:11:13 -0400

    Update app.py

--- final project/app.py
+++ final project/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import joblib
+import joblib 
 
 st.set_page_config(page_title="County Income Predictor", layout="wide")
 
